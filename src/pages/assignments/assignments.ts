dd9ff2a3558e89957588e5b4777aaa442b76f550
mmaartijn <mmaartijn@gmail.com>
2018-02-20 15:29:40 +0100

    Reset assignments.ts

--- src/pages/assignments/assignments.ts
+++ src/pages/assignments/assignments.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 import { Assignment } from './assignment.class';
 import { AssignmentsProvider } from './assignmentsProvider';
-import { IonicPage, NavController, NavParams, PopoverController, AlertController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { PopoverPage } from "./popover";
 
 @IonicPage()
@@ -16,56 +16,14 @@ export class AssignmentsPage {
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    public assignmentsProvider: AssignmentsProvider,
-    public popoverCtrl: PopoverController,
-    public alertCtrl: AlertController
+    public assignmentsProvider: AssignmentsProvider
   )
   {
     this.assignments = assignmentsProvider.getAssignments();
   }
 
   presentPopover($event) {
-    let popover = this.popoverCtrl.create(PopoverPage, { parentComponent: this });
-    popover.present({ ev: $event });
-  }
-
-  addAssignment() {
-    let alert = this.alertCtrl.create({
-      title: 'Add assignment',
-      inputs: [
-        { name: 'title', placeholder: 'Title' },
-        { name: 'content', placeholder: 'Content' }
-      ],
-      buttons: [
-        { text: 'Cancel', role: 'cancel' },
-        {
-          text: 'Add', handler: data => {
-            this.assignments.push(new Assignment(0, data.title, data.content));
-          }
-        }
-      ]
-    });
-    alert.present();
-  }
-
-  removeAssignments() {
-    let alert = this.alertCtrl.create({
-      title: 'Remove all checked?',
-      message: 'Remove all checked assignments? This cannot be undone.',
-      buttons: [
-        { text: 'Cancel', role: 'cancel' },
-        {
-          text: 'Delete', role: 'delete',
-          handler: () => {
-            let toRemove = this.assignments.filter(a => a.Checked);
-            for (let a of toRemove) {
-              this.assignments.splice(this.assignments.indexOf(a), 1);
-            }
-          }
-        }
-      ]
-    });
-    alert.present();
+    // TODO: Implement
   }
 }
 
